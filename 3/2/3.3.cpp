aad0359973158804ab5c6764425206da3588f5fc
xzhkpytf <IAreviev@gmail.com>
2025-07-17 16:10:26 +0500

    Update 3.3.cpp

--- 3/2/3.3.cpp
+++ 3/2/3.3.cpp
@@ -1,9 +1,15 @@
-	int a = 0;
-	int c = 0;
-  cin >> a;
-	while (c!=a) {
-		c++;
-		if(c*c < a){
-			cout << " " << c*c;
-		}
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+int a = 0;
+int c = 0;
+cin >> a;
+while (c!=a) {
+	c++;
+	if(c*c < a){
+		cout << " " << c*c;
 	}
+}
+}
