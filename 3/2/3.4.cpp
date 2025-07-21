95acc7cb68ed1c3e2b40e15d9af2e44bb4875693
xzhkpytf <IAreviev@gmail.com>
2025-07-21 15:28:35 +0500

    Create 3.4.cpp

---
+++ 3/2/3.4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{	
+char l = ' ';
+	int f = 0;
+
+	while (l != '0') {
+		cin >> l;
+		cout << endl;
+		f++;
+	}
+	cout << f-1;
+}
