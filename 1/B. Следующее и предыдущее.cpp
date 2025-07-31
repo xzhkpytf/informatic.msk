4b7b0a64fccfe94171ac14461168c81ba126d55a
xzhkpytf <IAreviev@gmail.com>
2025-07-31 15:58:25 +0500

    Create B. Следующее и предыдущее.cpp

---
+++ 1/B. Следующее и предыдущее.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+int one = 179;
+	
+cout << "The next number for the number " << one << " is " << one + 1 << endl;
+cout << "The previous number for the number " << one << " is " << one - 1 << endl;
+}
