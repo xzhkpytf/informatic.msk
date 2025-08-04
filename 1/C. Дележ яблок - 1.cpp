6c9270a21fc9dde0abc4e93f75a86400af73c8ec
xzhkpytf <IAreviev@gmail.com>
2025-08-04 15:31:13 +0500

    Update C. Дележ яблок - 1.cpp

--- 1/C. Дележ яблок - 1.cpp
+++ 1/C. Дележ яблок - 1.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 
 int main() {
-    // Считываем входные данные
     int N, K;
     cin >> N >> K;
 
-    // Вычисляем количество яблок, которое достанется каждому школьнику
     int apples_per_student = K / N;
 
-    // Выводим результат
     cout << apples_per_student;
 
     return 0;
