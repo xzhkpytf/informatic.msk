d0be4e2c70a8c74d0c3f05569d093f50b6b67b8d
xzhkpytf <IAreviev@gmail.com>
2025-10-16 16:13:07 +0500

    Update G. Число десятков двузначного числа.cpp

--- 1/G. Число десятков двузначного числа.cpp
+++ 1/G. Число десятков двузначного числа.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    
-    int count = 0;
-    while (n > 0) {
-        if (n % 10 == 0) {
-            count++;
-        }
-        n /= 10;
-    }
-    
-    cout << count << endl;
-    return 0;
+int main(){
+
+	int n;
+	cin >> n;
+
+	cout << n/10 << endl;
 }
