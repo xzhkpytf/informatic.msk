70058a26c2792d00aad6a26f0472aadbd6a5db3b
xzhkpytf <IAreviev@gmail.com>
2025-12-18 16:20:49 +0500

    Create H. Слон.cpp

---
+++ 2/H. Слон.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x1, y1, x2, y2 = 0;
+	cin >> x1 >> y1 >> x2 >> y2;
+
+
+	if (abs(x1 - x2) == abs(y1 - y2)) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+}
