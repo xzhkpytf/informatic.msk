1e83a6672f9d9718de9d510513b9bcfffbf79c11
xzhkpytf <IAreviev@gmail.com>
2026-01-19 16:27:14 +0500

    Create M. Фишки.cpp

---
+++ 2/M. Фишки.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int k = 0;
+	cin >> k;
+	if (k % 4 == 0 || k == 1) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+
+}
