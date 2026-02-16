3660ed166f67683296b9e8794f1c5529a10bf09c
xzhkpytf <IAreviev@gmail.com>
2026-02-16 16:21:58 +0500

    Create Q. Мороженое.cpp

---
+++ 2/Q. Мороженое.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    if (k==3 || k == 5 || k == 6 || k >=8) {
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
